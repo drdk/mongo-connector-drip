cfe55250803372a8cd9beefb09272da1ef9999d1
Kenneth Brøckner Barslund <kbba@dr.dk>
2016-11-10 14:33:59 +0100

    LOG.debug

--- mongo_connector/doc_managers/generic_http_doc_manager.py
+++ mongo_connector/doc_managers/generic_http_doc_manager.py
@@ -192,7 +192,7 @@ class DocManager(DocManagerBase):
 
     def _send_upsert(self, json):
         success = True
-        LOG.info("sending to ironmq")
+        LOG.debug("sending to ironmq")
         queue = self.ironmq.queue('test')
         queue.post(json)
         return success
