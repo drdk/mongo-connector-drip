358bd58eeb41f4335c389360fb3f2e84a9943779
Kenneth Brøckner Barslund <kbba@dr.dk>
2016-11-14 13:08:36 +0100

    Better.

--- mongo_connector/doc_managers/generic_http_doc_manager.py
+++ mongo_connector/doc_managers/generic_http_doc_manager.py
@@ -91,21 +91,15 @@ class DocManager(DocManagerBase):
 
     @wrap_exceptions
     def update(self, document_id, update_spec, namespace, timestamp):
-        messages = []
         db, coll = self._db_and_collection(namespace)
         message = self._doc_to_json(self._formatter.format_document(self.mongo[db][coll].find_one({"_id" : document_id})), str(document_id), 'U', timestamp)
-        messages.append(message)
-        jsonmessages = json.dumps(messages, default=json_util.default)
-        if (self._send_upsert(jsonmessages)):
+        if (self._send_upsert(message)):
             self.write_last_doc_timestamp(timestamp)
 
     @wrap_exceptions
     def upsert(self, doc, namespace, timestamp):
-        messages = []
         message = self._doc_to_json(self._formatter.format_document(doc), str(doc[self.unique_key]), 'C', timestamp)
-        messages.append(message)
-        jsonmessages = json.dumps(messages, default=json_util.default)
-        if (self._send_upsert(jsonmessages)):
+        if (self._send_upsert(message)):
             self.write_last_doc_timestamp(timestamp)
 
     @wrap_exceptions
@@ -116,8 +110,7 @@ class DocManager(DocManagerBase):
             while batch:
                 messages = []
                 messages.extend(batch)
-                jsonmessages = json.dumps(messages, default=json_util.default)
-                if (self._send_upsert(jsonmessages)):
+                if (self._send_upsert(messages)):
                     self.write_last_doc_timestamp(timestamp)
                 else:
                     break
@@ -128,11 +121,8 @@ class DocManager(DocManagerBase):
 
     @wrap_exceptions
     def remove(self, document_id, namespace, timestamp):
-        messages = []        
         message = self._doc_to_json(None, str(document_id), 'D', timestamp)
-        messages.append(message)
-        jsonmessages = json.dumps(messages, default=json_util.default)
-        if (self._send_upsert(jsonmessages)):
+        if (self._send_upsert(message)):
             self.write_last_doc_timestamp(timestamp)
 
     def write_last_doc_timestamp(self, timestamp):
@@ -191,9 +181,8 @@ class DocManager(DocManagerBase):
         }
         return message
 
-    def _send_upsert(self, json):
+    def _send_upsert(self, docData):
         success = True
-        for i in json:
-            LOG.debug(i)
-        self.ironMqQueue.post(*[for element in json)])
+        jsonOut = json.dumps(docData, default=json_util.default)
+        self.ironMqQueue.post(jsonOut)
         return success
